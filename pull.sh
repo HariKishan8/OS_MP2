2c26b571c110e85868491cabeaebfefb41a87d7b
kk <krishnakishore.srikonda@gmail.com>
2014-09-29 00:15:30 -0400

    adding pull script

---
+++ pull.sh
@@ -0,0 +1,9 @@
+# HariKishan8
+# kishorekk.8
+# https://github.com/HariKishan8/OS_MP2#os_mp2
+
+### do step 1 only for firs time you clone a repository
+
+#step1 git clone https://github.com/HariKishan8/OS_MP2.git
+
+git pull https://github.com/HariKishan8/OS_MP2.git
